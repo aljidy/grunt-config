1e40b5e4ee9202aedfd80d4db89e713d44db4c4e
Adam <abaj500@york.ac.uk>
2015-03-06 11:49:55 +0000

    Added skeleton php file

---
+++ eb/index.php
@@ -0,0 +1,20 @@
+<?php include './custom-page-head.php'; ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script src="http://cdn.jsdelivr.net/bxslider/4.1.1/jquery.bxslider.css"></script>
+<script src='http://cdn.jsdelivr.net/bxslider/4.1.1/jquery.bxslider.min.js'></script>
+
+<link href="./style.css" rel="stylesheet" />
+
+<script type="text/javascript">   
+</script>
+            
+
+<div class='custWrapper'>
+</div>
+
+<?php include './custom-page-foot.php'; ?>
+
+
+                
